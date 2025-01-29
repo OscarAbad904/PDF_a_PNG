a00d102530505f09c006542955de7ed9449e79e9
OscarAbad904 <sinnombre904@hotmail.com>
2025-01-29 22:24:02 +0100

    Versión terminada

--- PDF_a_PNG.py
+++ PDF_a_PNG.py
@@ -2,7 +2,7 @@ import sys
 import os
 from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
 from PyQt6 import uic
-import fitz  # PyMuPDF
+import fitz
 
 class PDFtoImagesApp(QMainWindow):
     def __init__(self):
